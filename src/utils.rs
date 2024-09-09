17c271471a90e7c702924e2f844f20b1b13764f6
Yassine <yassinebenarbia980@gmail.com>
2024-09-10 02:26:10 +0300

    chor: log now creates the log file instead of panicing if file doesn't exit

--- src/utils.rs
+++ src/utils.rs
@@ -10,11 +10,12 @@ pub trait StringFeatures {
 /// logs _data_ to a _file_ in a incremantive manner
 /// 
 /// Panics: 
-/// - if file does not exist
+/// - failed to create or write to the file
 pub fn log(data: &str, filename: &str) -> std::io::Result<std::fs::File>{
     let mut f = OpenOptions::new()
         .write(true)
         .append(true)
+        .create(true)
         .open(filename)?;
     writeln!(f, "{}", data)?;
     return std::io::Result::Ok(f)
